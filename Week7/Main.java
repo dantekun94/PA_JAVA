910ce566f1df30e046785959d4dbcf350486ab95
Nedelcu Răzvan-Cornel <dantekun94@users.noreply.github.com>
2020-05-26 20:41:44 +0300

    Add files via upload

---
+++ Week7/Main.java
@@ -0,0 +1,11 @@
+package Domain;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player player1 = new Player("NumeJucator");
+        Player player2 = new Player("NumeCelalaltJucator");
+
+
+    }
+}
---
+++ Week7/Token.java
@@ -0,0 +1,24 @@
+package Domain;
+
+public class Token {
+    private int value;
+
+    public Token(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "value=" + value +
+                '}';
+    }
+}
